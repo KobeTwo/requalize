a200e2241ed60d9c91d3a2614d7093f5f5f74395
Robert Schmidt <r.schmidt@dotsource.de>
2017-02-02 21:23:11 +0100

    initial spring boot project

---
+++ requalize-web/src/main/java/de/requalize/RequalizeWebApplication.java
@@ -0,0 +1,12 @@
+package de.requalize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequalizeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequalizeWebApplication.class, args);
+	}
+}
---
+++ requalize-web/src/test/java/de/requalize/RequalizeWebApplicationTests.java
@@ -0,0 +1,16 @@
+package de.requalize;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RequalizeWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
