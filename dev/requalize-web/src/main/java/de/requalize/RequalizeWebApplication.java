0115c7d1608c20698296e69d28ade7135e069fc9
Robert Schmidt <r.schmidt@dotsource.de>
2017-02-21 21:27:06 +0100

    added new release

--- dev/requalize-web/src/main/java/de/requalize/RequalizeWebApplication.java
+++ dev/requalize-web/src/main/java/de/requalize/RequalizeWebApplication.java
@@ -4,20 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.zkoss.zk.au.http.DHtmlUpdateServlet;
 import org.zkoss.zk.ui.http.DHtmlLayoutServlet;
-import org.zkoss.zk.ui.http.HttpSessionListener;
 
-@Configuration
-@ComponentScan("de.requalize")
-@EnableAutoConfiguration
+@SpringBootApplication
 public class RequalizeWebApplication {
 
 	public static void main(String[] args) {
@@ -32,21 +27,21 @@ public class RequalizeWebApplication {
 	}
 	
 	@Bean
-	public ServletRegistrationBean dHtmlLayoutServlet() {
+	public ServletRegistrationBean<DHtmlLayoutServlet> dHtmlLayoutServlet() {
 		Map<String, String> params = new HashMap<String, String>();
 		params.put("update-uri", "/zkau");
 		DHtmlLayoutServlet dHtmlLayoutServlet = new DHtmlLayoutServlet();
-		ServletRegistrationBean reg = new ServletRegistrationBean(dHtmlLayoutServlet, "*.zul");
+		ServletRegistrationBean<DHtmlLayoutServlet> reg = new ServletRegistrationBean<DHtmlLayoutServlet>(dHtmlLayoutServlet, "*.zul");
 		reg.setLoadOnStartup(1);
 		reg.setInitParameters(params);
 		return reg;
 	}
 
 	@Bean
-	public ServletRegistrationBean dHtmlUpdateServlet() {
+	public ServletRegistrationBean<DHtmlUpdateServlet> dHtmlUpdateServlet() {
 		Map<String, String> params = new HashMap<String, String>();
 		params.put("update-uri", "/zkau/*");
-		ServletRegistrationBean reg = new ServletRegistrationBean(new DHtmlUpdateServlet(), "/zkau/*");
+		ServletRegistrationBean<DHtmlUpdateServlet> reg = new ServletRegistrationBean<DHtmlUpdateServlet>(new DHtmlUpdateServlet(), "/zkau/*");
 		reg.setLoadOnStartup(2);
 		reg.setInitParameters(params);
 		return reg;
